e8c5a0fcfb327a756464c56fce80c512e8a121cd
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-08-04 15:26:02 -0500

    Completed documentation for the class.

--- TrackerLibrary/Models/TeamModel.cs
+++ TrackerLibrary/Models/TeamModel.cs
@@ -2,10 +2,24 @@
 
 namespace TrackerLibrary.Models
 {
+	/// <summary>
+	/// Represents a team within a tournament.
+        /// </summary>
 	public class TeamModel
 	{
+		/// <summary>
+		/// The unique identifier for each team.
+		/// </summary>
 		public int Id { get; set; }
+		
+		/// <summary>
+		/// A list of members for each team.
+		/// </summary>
 		public List<PersonModel> TeamMembers { get; set; } = new List<PersonModel>();
+		
+		/// <summary>
+		/// The team name for each team.
+		/// </summary>
 		public string TeamName { get; set; }
 	}
 }
