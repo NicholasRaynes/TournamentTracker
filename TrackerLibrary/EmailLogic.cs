0b6db8a7962d3919eccb0e202197c719d134f0e6
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-08-05 14:49:54 -0500

    Completed documentation for the class.

--- TrackerLibrary/EmailLogic.cs
+++ TrackerLibrary/EmailLogic.cs
@@ -3,13 +3,29 @@ using System.Net.Mail;
 
 namespace TrackerLibrary
 {
+	/// <summary>
+	/// Represents the functionality for sending an email.
+	/// </summary>
 	public static class EmailLogic
 	{
+		/// <summary>
+		/// Responsible for gathering email information for the second SendEmail method.
+		/// </summary>
+		/// <param name="toAddress">The address of the user/team.</param>
+		/// <param name="subject">The subject of the email.</param>
+		/// <param name="body">The body content of the email.</param>
 		public static void SendEmail(string toAddress, string subject, string body)
 		{
 			SendEmail(new List<string> { toAddress }, new List<string>(), subject, body);
 		}
 
+		/// <summary>
+		/// Responsible for sending an email to a user/team.
+		/// </summary>
+		/// <param name="toAddresses">The addresses of the team members.</param>
+		/// <param name="bccAddresses">The blind carbon copy addresses.</param>
+		/// <param name="subject">The subject of the email.</param>
+		/// <param name="body">The body content of the email.</param>
 		public static void SendEmail(List<string> toAddresses, List<string> bccAddresses, string subject, string body)
 		{
 			MailAddress fromMailAddress = new MailAddress(GlobalConfig.AppKeyLookup("senderEmail"), GlobalConfig.AppKeyLookup("senderDisplayName"));
