6a2334f85d4a7d8d5860491055a2e6f4d7676fa6
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-08-16 13:06:52 -0500

    Completed documentation for the interface.

--- TrackerUI/IPrizeRequester.cs
+++ TrackerUI/IPrizeRequester.cs
@@ -2,6 +2,9 @@
 
 namespace TrackerUI
 {
+	/// <summary>
+    	/// This interface represents a prize requester for the application.
+    	/// </summary>
 	public interface IPrizeRequester
 	{
 		void PrizeComplete(PrizeModel model);
