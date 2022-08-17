1e848e37899c68df750c7d2f8c04460b74e04b5f
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-08-17 14:02:00 -0500

    Completed documentation for the class.

--- TrackerUI/ITeamRequester.cs
+++ TrackerUI/ITeamRequester.cs
@@ -2,6 +2,9 @@
 
 namespace TrackerUI
 {
+	/// <summary>
+    	/// This interface represents a team requester for the application.
+    	/// </summary>
 	public interface ITeamRequester
 	{
 		void TeamComplete(TeamModel model);
